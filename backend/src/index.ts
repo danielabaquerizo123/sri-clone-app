cefbc207191bd0192d5cd0dce62a2377390f7f07
Daniela Baquerizo <dnbaquerizom@fafi.utb.edu.ec>
2026-05-25 12:22:30 -0500

    Fix production CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -42,11 +42,7 @@ const swaggerDocs = swaggerJsdoc(swaggerOptions);
 
 app.use(
   cors({
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:4173',
-      process.env.FRONTEND_URL || 'http://localhost:5173',
-    ],
+    origin: true,
     credentials: true,
   })
 );
